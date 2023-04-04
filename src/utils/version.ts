ca127f9f93c550af730a173d2198355971376f2b
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2023-04-04 16:24:33 +0200

    Refactor code

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -48,7 +48,11 @@ async function getNewVersionName(
   const isPrereleaseChannel = channel !== STABLE;
   const newBuildForPrerelease = core.getBooleanInput(NEW_BUILD_FOR_PRERELEASE);
 
-  if (isPrereleaseChannel && hasPrerelease(tagName) && newBuildForPrerelease) {
+  if (
+    isPrereleaseChannel &&
+    hasPrereleaseId(tagName) &&
+    newBuildForPrerelease
+  ) {
     kind = PRERELEASE;
   } else {
     kind = await getKindByPullRequestsLabels(tagCreatedAt);
@@ -74,7 +78,7 @@ async function getNewVersionName(
   }
 }
 
-function hasPrerelease(tagName: string): boolean {
+function hasPrereleaseId(tagName: string): boolean {
   if (tagName.startsWith(V)) {
     tagName = tagName.substring(1);
   }
