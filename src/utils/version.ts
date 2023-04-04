f00ebaf05fa222a63c053a438df7893fe91d0de5
Miguel Ripoll <3296866+MiguelRipoll23@users.noreply.github.com>
2023-04-04 16:22:06 +0200

    Fix prerelease bump to sable

--- src/utils/version.ts
+++ src/utils/version.ts
@@ -42,19 +42,19 @@ async function getNewVersionName(
   tagName: string,
   tagCreatedAt: string
 ): Promise<string | null> {
-  const newBuildForPrerelease = core.getBooleanInput(NEW_BUILD_FOR_PRERELEASE);
-
   let kind: "unknown" | "major" | "minor" | "patch" | "prerelease" = UNKNOWN;
 
-  if (hasPrerelease(tagName) && newBuildForPrerelease) {
+  const channel = core.getInput(CHANNEL, { required: true });
+  const isPrereleaseChannel = channel !== STABLE;
+  const newBuildForPrerelease = core.getBooleanInput(NEW_BUILD_FOR_PRERELEASE);
+
+  if (isPrereleaseChannel && hasPrerelease(tagName) && newBuildForPrerelease) {
     kind = PRERELEASE;
   } else {
     kind = await getKindByPullRequestsLabels(tagCreatedAt);
   }
 
   core.debug("Kind: " + kind);
-
-  const channel = core.getInput(CHANNEL, { required: true });
 
   switch (kind) {
     case MAJOR:
